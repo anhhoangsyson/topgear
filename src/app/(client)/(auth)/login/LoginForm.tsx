1a89574fbb6f9600c5d2eec7dbb02ed2f10d123a
nguyencongtoan1 <congtoannguyen123490@gmail.com>
2025-05-05 22:56:31 +0700

    update

--- src/app/(client)/(auth)/login/LoginForm.tsx
+++ src/app/(client)/(auth)/login/LoginForm.tsx
@@ -15,11 +15,11 @@ export default function LoginForm() {
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const router = useRouter(); // Create an instance of the router
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
 
   // check if user is navigated form other page with message in url
   useEffect(() => {
-    const message = searchParams.get("message")
+    const message = searchParams.get("message");
     if (message) {
       toast({
         title: "Thông báo",
@@ -28,7 +28,7 @@ export default function LoginForm() {
         variant: "default",
       });
     }
-  }, [searchParams])
+  }, [searchParams]);
   // ✅ config React Hook Form with Zod Resolver
   const {
     register,
@@ -45,16 +45,19 @@ export default function LoginForm() {
 
     try {
       // send req to server
-      const response = await fetch("https://top-gear-be.vercel.app/api/v1/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: data.email,
-          password: data.password,
-        }),
-      });
+      const response = await fetch(
+        "https://top-gear-be.vercel.app/api/v1/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: data.email,
+            password: data.password,
+          }),
+        }
+      );
 
       const contentType = response.headers.get("content-type");
       if (contentType && contentType.includes("application/json")) {
@@ -65,7 +68,6 @@ export default function LoginForm() {
             description: "Chào mừng thượng đế đến với hệ thống Top Gear!",
           });
 
-
           // save data user (ex: token) in localStorage
           // localStorage.setItem("authToken", result.data.token);
           // khong luu o localStorage vi co the bi xoa
@@ -74,7 +76,7 @@ export default function LoginForm() {
           // document.cookie = `accessToken=${result.data.token}; path=/; max-age=36000; secure; samesite=strict`;
 
           // luu ca token o trong next server
-          const authRes = await fetch('/api/auth/', {
+          const authRes = await fetch("/api/auth/", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
@@ -82,10 +84,10 @@ export default function LoginForm() {
             body: JSON.stringify({
               accessToken: result.data.token,
             }),
-          })
-          const authData = await authRes.json()
-          console.log('authData', authData);
-          
+          });
+          const authData = await authRes.json();
+          console.log("authData", authData);
+
           // naviate /
           router.push("/");
         } else {
@@ -97,10 +99,11 @@ export default function LoginForm() {
         throw new Error("Server trả về định dạng không mong đợi");
       }
     } catch (error) {
-      setErrorMessage(error as string);
+      const errMessage = error instanceof Error ? error.message : String(error);
+      setErrorMessage(errMessage);
       toast({
         title: "Lỗi",
-        description: error as string || "Đăng nhập thất bại",
+        description: errMessage || "Đăng nhập thất bại",
         variant: "destructive",
       });
     } finally {
@@ -136,7 +139,9 @@ export default function LoginForm() {
             className="mt-2 p-2 w-full border rounded bg-white focus:bg-white focus:border-gray-700 outline-none"
           />
           {errors.password && (
-            <p className="text-red-500 text-[13px]">{errors.password.message}</p>
+            <p className="text-red-500 text-[13px]">
+              {errors.password.message}
+            </p>
           )}
         </div>
 
